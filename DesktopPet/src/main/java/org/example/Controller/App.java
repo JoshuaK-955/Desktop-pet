db7e55656dc9ac5926b02acaf685adfe14cb68b0
JoshuaK-java <jkal955@icloud.com>
2025-05-17 13:46:01 -0700

    progress in popup.java

--- DesktopPet/src/main/java/org/example/Controller/App.java
+++ DesktopPet/src/main/java/org/example/Controller/App.java
@@ -1,7 +1,13 @@
 package org.example.Controller;
 
+import org.example.View.GUI;
+import org.example.View.popup;
+
+import static javafx.application.Application.launch;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        launch(popup.class);
     }
 }
