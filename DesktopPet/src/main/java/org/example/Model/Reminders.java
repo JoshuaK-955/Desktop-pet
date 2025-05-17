3e62715d6f3ea9a8f4985c74127e00a5b40dd059
Matthew <matthewpark2006@gmail.com>
2025-05-17 14:13:27 -0700

    Reminader setup

--- DesktopPet/src/main/java/org/example/Model/Reminders.java
+++ DesktopPet/src/main/java/org/example/Model/Reminders.java
@@ -1,4 +1,27 @@
 package org.example.Model;
 
+import java.util.Random;
+
 public class Reminders {
+    String[] remindersArray = new String[10];
+    int arraySize = 0;
+
+    public Reminders() {
+        remindersArray[0] = "Did you eat today?";
+        remindersArray[1] = "Did you drink water?";
+        remindersArray[2] = "Did you workout?";
+        remindersArray[3] = "Have user generate their own questions to ask";
+        arraySize = 4;
+    }
+
+    public String getRemindersArray() {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(remindersArray.length);
+        return remindersArray[randomIndex];
+    }
+
+    public void setRemindersArray(String newReminder) {
+        arraySize++;
+        remindersArray[arraySize] = newReminder;
+    }
 }
